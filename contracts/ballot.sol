f692144413add4c0751ab1b9f037f6711675f446
Friðrik Hjálmarsson <fridrik14@ru.is>
2018-05-10 14:37:00 +0000

    breyta contract

--- contracts/ballot.sol
+++ contracts/ballot.sol
@@ -2,14 +2,16 @@ pragma solidity ^0.4.17;
 
 contract ElectionCreation {
     address[] public deployedBallots;
-    
-    function createElection(bytes32[] candidates, bytes32[] district) public {
+    /*
+        Creates a set of smart contracts with a list of candidates for each voting district given
+    */
+    constructor (bytes32[] candidates, bytes32[] district) public {
         for(uint i = 0; i < district.length; i++){
             address newBallot = new Ballot(candidates, district[i], msg.sender);
             deployedBallots.push(newBallot);
         }
     }
-    
+    //Returns the addresses of each of the smart contracts created and deployed
     function getDeployedBallots() public view returns(address[]) {
         return deployedBallots;
     }
@@ -22,6 +24,7 @@ contract Ballot {
     struct Proposal {
         bytes32 name;
         uint voteCount;
+        uint creationDate;
     }
 	
     Proposal[] public proposals;
@@ -42,43 +45,43 @@ contract Ballot {
     constructor (bytes32[] proposalNames, bytes32 district, address creator) public {
         chairperson = creator;
         votingDistrict = district;
+        
         // For each of the provided proposal names,
         // create a new proposal object and add it
         // to the end of the array.
         for (uint i = 0; i < proposalNames.length; i++) {
             proposals.push(Proposal({
                 name: proposalNames[i],
-                voteCount: 0
+                voteCount: 0,
+                creationDate: now
             }));
         }
     } 
     function vote(uint proposal) public{
         //Check if voter has voted already
         require(!voters[msg.sender]);
-        //Increase selected proposal voteCount by 1
-        proposals[proposal].voteCount += 1;
-        //Set voter as already voted
-        voters[msg.sender] = true;
-    }
-
-    function winningProposal() public restricted view
-            returns (uint winProposal)
-    {
-        uint winningVoteCount = 0;
-        for (uint p = 0; p < proposals.length; p++) {
-            if (proposals[p].voteCount > winningVoteCount) {
-                winningVoteCount = proposals[p].voteCount;
-                winProposal = p;
-            }
+        /*After 30 seconds have passed, the election should be over and therefore no one should be 
+          allowed to vote after the time limit has been reached. The time limit can be changed for 
+          each election which is created.*/
+        if(now >= proposals[proposal].creationDate + 30 seconds){
+            revert();   
         }
+            //Increase selected proposal voteCount by 1
+            proposals[proposal].voteCount += 1;
+            //Set voter as already voted
+            voters[msg.sender] = true;
+    }
+    //Returns name of candidate from index
+    function getCandidateName(uint index) public restricted view
+            returns (bytes32)
+    {
+        return proposals[index].name;
+    }
+    //Returns the amount of votes the candidate at index has recieved in this particular voting district
+    function getVoteCount(uint index) public view 
+            returns (uint)
+    {
+        return proposals[index].voteCount;
     }
 
-    // Calls winningProposal() function to get the index
-    // of the winner contained in the proposals array and then
-    // returns the name of the winner
-    function winnerName() public restricted view
-            returns (bytes32 winName)
-    {
-        winName = proposals[winningProposal()].name;
-    }
 }
